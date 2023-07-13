6dae766a4a098b84eb40ac2724166c6363bdf52e
Kennjenga <kinyagia10@gmail.com>
2023-07-13 03:21:18 -0700

    printf script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -use printf compile without warning when using the -Wall gcc option
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
