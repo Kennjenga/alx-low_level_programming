21baeb673712e42c28cc91ad9c76809a5aecca8d
Kennjenga <kinyagia10@gmail.com>
2023-07-13 03:06:08 -0700

    first C script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - a program that prints a line with the put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
+
