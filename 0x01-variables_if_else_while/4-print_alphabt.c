f1bbc2a127a7ba8b936f344e56aca848b54fd412
Kennjenga <kinyagia10@gmail.com>
2023-07-14 06:18:36 -0700

    alphabets except e && q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print all except q and e
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n < 123)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
