d1f35e3399c745c3c362fc8d122dac55eddaf461
Kennjenga <kinyagia10@gmail.com>
2023-07-14 06:04:50 -0700

    all alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - alphabets
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+	int x = 65;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	while (x < 91)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
