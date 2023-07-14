4c4c9f81c57c8f0491617b1e1de094434dcc201d
Kennjenga <kinyagia10@gmail.com>
2023-07-14 12:22:00 -0700

    first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main -  lowercase alphabet in reverse
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = (b + 1); d < 58; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
