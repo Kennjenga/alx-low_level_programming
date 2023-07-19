25ff818e515fd8f2650ec0d72aeb06dfc5c59da3
Kennjenga <kinyagia10@gmail.com>
2023-07-19 02:29:41 -0700

    first 50 fib seq

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * fib - fibonnaci seq ( 0,1,1,2,3,5....)
+ * @n: num of sequence
+ *
+ *Return: seq
+ */
+int fib(int n)
+{
+	int first = 0, second = 1, next, i;
+
+	/*printf("Fibonacci Series: ");*/
+	for (i = 0; i < n; i++)
+	{
+		if (i <= 1)
+		{
+			next = i;
+		} else
+		{
+			next = first + second;
+			first = second;
+			second = next;
+		}
+		printf("%d, ", next);
+	}
+	printf("\n");
+}
+/**
+ * main - main func
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	fib(50);
+	return (0);
+}
