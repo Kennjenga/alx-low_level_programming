ef198b55d2ecd476d14b8e27238aca6917c0abb2
Kennjenga <kinyagia10@gmail.com>
2023-07-27 14:54:51 -0700

    function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,5 +19,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 
 #endif /*MAIN_H*/
