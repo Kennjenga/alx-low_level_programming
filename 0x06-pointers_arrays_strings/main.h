36215b7f168ab50bab7206ddbbcb2dd5b5c9dd8f
Kennjenga <kinyagia10@gmail.com>
2023-07-27 13:47:36 -0700

    a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * main - maun func
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ *@c: parameter
+ *return: nothing
+ */
+int _putchar(char c);
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif /*MAIN_H*/
