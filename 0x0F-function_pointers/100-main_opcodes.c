954dbcd0cbbb97dc3e42433b980afc9965985ba6
Kennjenga <108663105+Kennjenga@users.noreply.github.com>
2023-08-18 23:48:01 +0300

    program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,13 +1,3 @@
-/**
- * This program prints the opcodes of its own code.
- * 
- * @param argc The `argc` parameter is the number of command-line arguments passed to the program.
- * @param argv The `argv` parameter is an array of strings, where each string represents a command-line
- * argument passed to the program. In this case, `argv` will contain two elements: `argv[0]` will be
- * the name of the program itself, and `argv[1]` will be
- * 
- * @return The main function is returning an integer value of 0, indicating success.
- */
 #include <stdio.h>
 #include <stdlib.h>
 
